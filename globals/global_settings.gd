cab0140aad72b072d8c7412b6be37bbb4c36d30c
danielktaylor <daniel-k-taylor@users.noreply.github.com>
2024-10-05 22:52:00 -0700

    Update font fallback reference and version

--- globals/global_settings.gd
+++ globals/global_settings.gd
@@ -7,7 +7,7 @@ signal settings_changed_Language
 
 const ReleaseLoggingEnabled = false # If true, log even on release builds.
 const UseAzureServerAlways = false # If true, always defaults to the azure server. Otherwise release=Azure, dev=local.
-const ClientVersionString : String = "241005.1430" # YYMMDD.HHMM
+const ClientVersionString : String = "241005.2230" # YYMMDD.HHMM
 const ReplayVersion = 1 # Increment this when you break replay compatibility.
 
 const PlayfabTitleId = "57B37"
